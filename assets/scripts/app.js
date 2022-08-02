30be560c6e92316c9bc7a109cd8a4edba328bd7d
Stav <stavush93@gmail.com>
2022-08-02 23:24:13 +0300

    display movie inputs on UI

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,8 +6,33 @@ const closeMovieModalBtn = addMovieModal.querySelector(".btn--passive");
 const addMovieBtn = addMovieModal.querySelector(".btn--success");
 const userInputs = addMovieModal.querySelectorAll("input");
 
-
 const movies = [];
+
+const updateUi = () => {
+  const startSection = document.getElementById("entry-text");
+  if (movies.length === 0) {
+    startSection.style.display = "block";
+  } else {
+    startSection.style.display = "none";
+  }
+};
+
+const addNewMovieElement = ({title, image, rating}) => {
+  const newMovieEl = document.createElement("li")
+  newMovieEl.className = "movie-element";
+  newMovieEl.innerHTML =
+    `
+    <div class="movie-element__image"> 
+      <img src="${image}" alt="${title}"></img>
+    </div>
+    <div class="movie-element__info">
+      <h1>${title}</h1>
+      <p>${rating}/5</p>
+    </div>
+    `;
+    const listRoot = document.getElementById("movie-list")
+    listRoot.append(newMovieEl)
+}
 
 const toggleBackdrop = () => {
   //toggle - adds or removes the class base on its current state
@@ -24,7 +49,7 @@ const clearMovieInputs = () => {
   for (const userInp of userInputs) {
     userInp.value = "";
   }
-}
+};
 
 const backdropClickHandler = () => {
   toggleMovieModal();
@@ -46,19 +71,21 @@ const addMovieBtnHandler = () => {
     +ratingValue < 1 ||
     +ratingValue > 5
   ) {
-    alert("Please enter a number between 1 and 5")
+    alert("Please enter a number between 1 and 5");
   }
 
   const newMovie = {
     title: titleValue,
     image: imageValue,
-    rating: ratingValue
-  }
+    rating: ratingValue,
+  };
 
   movies.push(newMovie);
-  console.log(movies)
+  console.log(movies);
   toggleMovieModal();
   clearMovieInputs;
+  addNewMovieElement(newMovie)
+  updateUi();
 };
 
 openMovieModalBtn.addEventListener("click", toggleMovieModal);
